f8edc82921fb583f83ab376447e762019b7e68b9
ningyuan-ww <adidididi@icloud.com>
2021-05-09 16:49:19 +0800

    ???
    maybe uploaded the new gifs

---
+++ yyLoadHeaderFooter.js
@@ -0,0 +1,63 @@
+function loadHeaderFooter() {
+    fetch("yyHeader.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.querySelector("header").innerHTML = data;
+            navbarTogglerInit();
+        });
+
+    fetch("yyFooter.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.querySelector("footer").innerHTML = data;
+        });
+
+    fetch("yyTop.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+
+            const myBtn = document.getElementById("myBtn");
+
+            myBtn.innerHTML = data;
+
+            // When the user scrolls down 20px from the top of the document, show the button
+            window.onscroll = scrollFunction;
+
+            function scrollFunction() {
+                if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                    myBtn.style.display = "block";
+                } else {
+                    myBtn.style.display = "none";
+                }
+            }
+
+            myBtn.addEventListener("click", topFunction);
+
+        });
+}
+
+function navbarTogglerInit() {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarMenu = document.querySelector(".navbar ul");
+    navbarToggler.addEventListener("click", () => {
+        navbarToggler.classList.toggle("open-navbar-toggler");
+        navbarMenu.classList.toggle("open");
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadHeaderFooter();
+});
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
