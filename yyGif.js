8fa62314a42e1effbe295d211a2feba866d081d6
Yaoyi Chen <29800192+yaoyic@users.noreply.github.com>
2021-06-27 23:18:52 +0200

    Gif to video; using javascript to control the playing order.

---
+++ yyGif.js
@@ -0,0 +1,28 @@
+var gifArray;
+
+document.addEventListener('DOMContentLoaded', function() {
+    let gifCollection = document.getElementsByClassName("gif");
+    gifArray = [].slice.call(gifCollection);
+    gifArray.sort(function(a, b) { 
+        return a.id.localeCompare(b.id);
+    });
+    gifArray.forEach((gifElement, index) => 
+        { gifElement.onended = delayAndPlayNextGif; }
+    );
+    playNextGif();
+});
+
+function playNextGif() {
+    playNextGif.counter = playNextGif.counter || 0; // black magic
+    gifArray[playNextGif.counter].play();
+    playNextGif.counter += 1;
+    if(playNextGif.counter >= gifArray.length) {
+        playNextGif.counter = 0;
+    }
+}
+
+function delayAndPlayNextGif() {
+    setTimeout(function(){
+        playNextGif();
+    }, 2000);
+};
